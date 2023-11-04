ad9bbdfcccce71314e715f193737b4285f7c3808
Younes Karimi <younes@psu.edu>
2023-11-03 20:29:35 -0400

    added url domain extractor

--- pattern_matching.py
+++ pattern_matching.py
@@ -1,8 +1,10 @@
 __author__ = "Younes Karimi"
 __email__ = "younes@psu.edu"
-__description__ = "This file contains functions for extracting pieces of text that match a specific syntax or pattern"
+__description__ = """This file contains functions for extracting pieces of text 
+                        that match a specific syntax or pattern"""
 
 import re
+from urllib.parse import urlparse
 
 def has_ssn(text):
     return True if len(re.findall(r'((?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4})', text)) > 0 else False
@@ -16,3 +18,12 @@ def has_phone(text):
         if (phone.find(' ') > 0) or (phone.find('-') > 0):
             return True
     return False
+
+def extract_domain(url):
+    """
+    Extracts domain from URL that has scheme (http, https). If no scheme is 
+    present, returns an empty string. If a URL has subdomains, this function
+    cannot extract the main domain/website.
+    """
+    url = urlparse(url).netloc
+    return url.split('www.')[1] if url.find('www.') >= 0 else url
